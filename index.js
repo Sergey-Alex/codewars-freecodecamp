35c7299931f68d08dc7615d34c24100c973cf9c4
Sergey-Alex <stroitelevsergeyalex@gmail.com>
2022-01-30 21:49:48 +0300

    F:\Project\freecodecamp>git commit -m rec

--- index.js
+++ index.js
@@ -334,3 +334,16 @@
 //     }
 // }
 // console.log(randomRange(20, 30))
+
+// function countdown(n){
+//     if (n < 1) {
+//         return []
+//     } else {
+//         const arr = countdown(n-1)
+//         arr.unshift(n)
+//         return arr
+//     }
+//
+// }
+//
+// console.log(countdown(5))
