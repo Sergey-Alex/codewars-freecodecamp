6ad4b6dd2c1e290353bb1e13caa1f69914926884
Sergey-Alex <strotelevsergwyalex@gmail.com>
2022-06-26 23:11:51 +0300

    arrayDiff

--- index.js
+++ index.js
@@ -502,4 +502,14 @@
 
 function arrayDiff(a, b) {
 
+    if (a.length !== 0 && b.length !== 0) {
+      return   a.filter(el => !b.includes(el))
+    }
+    if (a.length === [] ){
+        return   b
+    } if (b.length === [] ){
+        return   a
+    }
 }
+
+console.log((arrayDiff([1, 2, 2], [2])))
